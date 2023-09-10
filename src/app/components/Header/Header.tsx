28d10d70e32b928323f60dc755ada4c4d0ca325b
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-09-10 21:34:50 +0200

    coomit

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import ThemeSwitcher from "./ThemeSwitcher";
 
 const Header = () => {
   return (
-    <header className="bg-white dark:bg-black z-50 w-full fixed top-0 left-0 h-20 flex items-center justify-between">
+    <header className="bg-white dark:bg-black shadow-md dark:shadow-sky-400 z-50 w-full fixed top-0 left-0 h-20 flex items-center justify-between">
       <div className="container flex justify-between items-center">
         <Logo />
         <NavMenu />
