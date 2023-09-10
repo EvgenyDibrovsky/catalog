57aa33c977da9387c9ac5498aa5815622bdfbe64
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-09-10 11:00:08 +0200

    commit

--- src/app/catalog/developers/loading.tsx
+++ src/app/catalog/developers/loading.tsx
@@ -1,7 +1,19 @@
+"use client";
+import { Puff } from "react-loader-spinner";
+
 export default function LoadingCompany() {
   return (
     <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <h1>Loading ...</h1>
+      <Puff
+        height="80"
+        width="80"
+        radius={1}
+        color="#0064b0"
+        ariaLabel="puff-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
     </div>
   );
 }
