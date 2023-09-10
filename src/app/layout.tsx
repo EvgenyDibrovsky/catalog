b7431f1fd869708aa84f276de5a198d3a160970f
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-09-10 21:25:46 +0200

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import Providers from "./providers.js";
 const poppins = Poppins({
   display: "swap",
   weight: ["400", "500", "600", "700"],
-  subsets: ["latin"]
+  subsets: ["latin"],
 });
 export const metadata: Metadata = {
   title: "New Project",
@@ -21,18 +21,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-
-    <html lang="en">
-
-      <body className={poppins.className}>
-        <Providers>
-          <Header />
-          <main className="min-h-screen">{children}</main>
-          <Footer />
-        </Providers>
-      </body>
-
-    </html>
-
+  
+    <body className={poppins.className}>
+      <Providers>
+        <Header />
+        <main className="min-h-screen">{children}</main>
+        <Footer />
+      </Providers>
+    </body>
   );
 }
--- src/app/providers.js
+++ src/app/providers.js
@@ -1,17 +1,10 @@
 "use client";
 import { ThemeProvider } from "next-themes";
-import { useState, useEffect } from "react";
 
 export default function Providers({ children }) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
   return (
     <ThemeProvider attribute="class" defaultTheme="light">
-      {mounted ? children : null}
+      {children}
     </ThemeProvider>
   );
 }
