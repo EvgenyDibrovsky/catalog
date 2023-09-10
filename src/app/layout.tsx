cdb0870f505e16501c7eb8230ab8a6e07fabb765
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-09-10 12:08:19 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={poppins.className} >
         <Header/>
-        <main className='h-screen'>
+        <main>
         {children}
         </main>
         <Footer/>
