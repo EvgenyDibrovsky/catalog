9e1042fe678e2a90c26b540a43d5bfdea2bcd888
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-09-16 11:12:46 +0200

    commit

--- src/components/CardsListCompany/CardsListCompanyItem.jsx
+++ src/components/CardsListCompany/CardsListCompanyItem.jsx
@@ -1,17 +1,17 @@
 export default function CardsListCompanyItem({ image, title, text, link }) {
   return (
-    <li className="group h-auto relative flex flex-col justify-between shadow-md dark:shadow-white rounded-md overflow-hidden">
+    <li className="flex shadow-md dark:shadow-white rounded-md overflow-hidden duration-200 hover:shadow-sky-400 dark:hover:shadow-yellow-500 group">
       <a href={link} target="_blank" rel="noopener noreferrer">
-        <div className="flex items-center justify-center">
-          <div className="w-3/12 flex flex-col gap-2">
+        <div className="p-4">
+          <div className="flex item-center justify-between gap-2 mb-4">
             <img
               src={process.env.PUBLIC_URL + image}
               alt={title}
-              className="h-40 p-4 "
+              className="w-auto h-12 duration-200 group-hover:scale-110"
             />
+            <h3 className="flex items-center font-bold">{title}</h3>
           </div>
-          <div className="w-9/12 flex flex-col gap-2">
-            <h3 className="font-bold">{title}</h3>
+          <div className="flex flex-col gap-2">
             <p className=" text-black  dark:text-white  ">{text}</p>
           </div>
         </div>
--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -9,7 +9,7 @@ export default function Logo() {
     <>
       <Link to="/">
         <div className="flex gap-4 items-center">
-          <SiHomebridge className="text-colorLogo dark:text-yellow-500 transition-all duration-500 text-[3rem] z-50" />
+          <SiHomebridge className="text-colorLogo dark:text-yellow-500 transition-all duration-500 text-[2rem] z-50" />
           <div className="hidden sm:flex flex-col z-50">
             <p className="text-[0.6rem] font-semibold text-black dark:text-white transition-all duration-200">
               {t('header.logo.logo_text_1')}
