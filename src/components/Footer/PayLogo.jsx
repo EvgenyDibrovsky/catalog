930b39c69af48cd07d5219c3ecaa574907fdd8a4
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-09-18 21:29:19 +0200

    commit

--- src/components/Footer/PayLogo.jsx
+++ src/components/Footer/PayLogo.jsx
@@ -6,14 +6,14 @@ export default function PayLogo() {
   return (
     <>
       <ul className="flex gap-2 items-center  justify-center lg:justify-end">
-        <li className="flex items-center justify-center bg-iconBg dark:bg-iconBgDark h-10 px-2 rounded-sm">
-          <img src={paypal} alt="paypal" className="h-6" />
+        <li className="flex items-center justify-center bg-iconBg dark:bg-iconBgDark h-6 px-2 rounded-sm">
+          <img src={paypal} alt="paypal" className="h-4" />
         </li>
-        <li className="flex items-center justify-center bg-iconBg dark:bg-iconBgDark h-10 px-2 rounded-sm">
-          <img src={mastercard} alt="paypal" className="h-8" />
+        <li className="flex items-center justify-center bg-iconBg dark:bg-iconBgDark h-6 px-2 rounded-sm">
+          <img src={mastercard} alt="paypal" className="h-6" />
         </li>
-        <li className="flex items-center justify-center bg-iconBg dark:bg-iconBgDark h-10 px-2 rounded-sm">
-          <img src={visa} alt="paypal" className="h-10" />
+        <li className="flex items-center justify-center bg-iconBg dark:bg-iconBgDark h-6 px-2 rounded-sm">
+          <img src={visa} alt="paypal" className="h-8" />
         </li>
       </ul>
     </>
--- src/components/Footer/SocialLinks.jsx
+++ src/components/Footer/SocialLinks.jsx
@@ -9,17 +9,17 @@ export default function SocialBtn() {
       <ul className="list-none flex flex-wrap  justify-center lg:justify-end gap-2 mb-4 ">
         <li className="cursor-pointer inline-flex justify-center items-center p-1 rounded-sm bg-iconBg dark:bg-iconBgDark text-sky-600 focus:outline-none  text-[1.5rem] group">
           <a href="https://#" target="_blank" rel="noopener noreferrer" aria-label={t('social-links.telegram')}>
-            <BsTelegram className="w-6 h-6 duration-200 group-hover:scale-95" />
+            <BsTelegram className="w-4 h-4 duration-200 group-hover:scale-95" />
           </a>
         </li>
         <li className="cursor-pointer inline-flex justify-center items-center p-1 rounded-sm bg-iconBg dark:bg-iconBgDark text-blue-900 focus:outline-none text-[1.5rem] group">
           <a href="https://#" target="_blank" rel="noopener noreferrer" aria-label={t('social-links.facebook')}>
-            <BsFacebook className="w-6 h-6 duration-200 group-hover:scale-95" />
+            <BsFacebook className="w-4 h-4 duration-200 group-hover:scale-95" />
           </a>
         </li>
         <li className="cursor-pointer inline-flex justify-center items-center p-1 rounded-sm bg-iconBg dark:bg-iconBgDark text-sky-700 focus:outline-none text-[1.5rem] group">
           <a href="https://#" target="blank" aria-label={t('social-links.linkedin')}>
-            <BsLinkedin className="w-6 h-6 duration-200 group-hover:scale-95" />
+            <BsLinkedin className="w-4 h-4 duration-200 group-hover:scale-95" />
           </a>
         </li>
       </ul>
