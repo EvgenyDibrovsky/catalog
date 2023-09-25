40e8376f5dc1b5af7b399369246b361b0d0a4f71
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-09-25 19:59:47 +0200

    commit

--- src/components/PageSettings/PageTitle.jsx
+++ src/components/PageSettings/PageTitle.jsx
@@ -1,7 +1,3 @@
 export default function PageTitle({ title }) {
-  return (
-    <h1 className="relative text-[1.25rem] text-center after:content-[''] after:absolute after:-bottom-1 after:left-1/2 after:-translate-x-1/2 after:w-full after:h-[2px] after:bg-titleLine dark:after:bg-yellow-500">
-      {title}
-    </h1>
-  );
+  return <h1 className="flex mb-2 text-[1.5rem] font-bold">{title}</h1>;
 }
--- src/components/PageSettings/Titles.jsx
+++ src/components/PageSettings/Titles.jsx
@@ -4,7 +4,7 @@ import SubTitle from 'components/PageSettings/SubTitle';
 export default function Titles({ title, subTitle }) {
   return (
     <>
-      <div className="flex flex-col gap-2 lg:gap-0 lg:flex-row justify-between items-baseline mb-10">
+      <div className="flex justify-between items-baseline relative mb-10 border-b-2 border-titleLine dark:border-yellow-500">
         <PageTitle title={title} />
         <SubTitle subTitle={subTitle} />
       </div>
