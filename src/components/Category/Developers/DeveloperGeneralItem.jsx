0340177d06cd2182ee258d00060674c7874e7a35
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-10-02 13:07:04 +0200

    commit

--- src/components/Category/Developers/DeveloperGeneralItem.jsx
+++ src/components/Category/Developers/DeveloperGeneralItem.jsx
@@ -1,13 +1,14 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import useCurrentLanguage from '../../../Hooks/useCurrentLanguage';
+import { useTranslation } from 'react-i18next';
 
 export default function DeveloperGeneralItem() {
   const { developerId } = useParams();
   const [data, setData] = useState(null);
   const currentLanguage = useCurrentLanguage();
   const [activeTab, setActiveTab] = useState('projects');
-
+  const { t } = useTranslation();
   useEffect(() => {
     import(`../../../db/catalog/bd-developers/developer_${developerId}.json`)
       .then(({ default: developer }) => {
@@ -31,7 +32,7 @@ export default function DeveloperGeneralItem() {
 
   return (
     <div className="flex gap-4">
-      <div className="w-9/12 flex flex-col gap-4">
+      <div className="w-8/12 flex flex-col gap-4">
         <div className=" bg-white dark:bg-darkBgContent p-5">
           <h1 className="text-black dark:text-white font-bold text-[1.2rem]">{contentTitle}</h1>
           <p className="text-black dark:text-white">{contentDescription}</p>
@@ -63,50 +64,47 @@ export default function DeveloperGeneralItem() {
           </div>
         </div>
       </div>
-      <div className="w-3/12 flex flex-col gap-4">
-        <div className="bg-white dark:bg-darkBgContent p-5 ">
-          {logo && <img src={process.env.PUBLIC_URL + logo} alt={name_company} className="h-5 w-auto mb-2 sm:mb-0" />}
-          {name_company && <p className="text-black dark:text-white font-bold">{name_company}</p>}
+      <div className="w-4/12 flex flex-col gap-4">
+        <div className="bg-white dark:bg-darkBgContent flex flex-col gap-2 p-5 ">
+          {logo && <img src={process.env.PUBLIC_URL + logo} alt={name_company} className="h-10 object-contain mb-2 sm:mb-2 mr-auto" />}
+
+          {name_company && <p className="text-black dark:text-white font-bold text-[1.1rem] mb-1">{name_company}</p>}
+
           {nip && (
             <div className="flex gap-2">
-              <p className="text-black dark:text-white font-bold">Nip:</p>
+              <p className="text-black dark:text-white font-bold w-3/12">{t('pages_category.developers.page_item.nip')}:</p>
               <p className="text-black dark:text-white">{nip}</p>
             </div>
           )}
           {link_site && (
             <div className="flex gap-2">
-              <p className="text-black dark:text-white font-bold">Сайт:</p>
+              <p className="text-black dark:text-white font-bold w-3/12">{t('pages_category.developers.page_item.www')}:</p>
               <p className="text-black dark:text-white">{link_site}</p>
             </div>
           )}
           {address && (
             <div className="flex gap-2">
-              <p className="text-black dark:text-white font-bold">Адрес:</p>
+              <p className="text-black dark:text-white font-bold w-3/12">{t('pages_category.developers.page_item.address')}:</p>
               <p className="text-black dark:text-white">{address}</p>
             </div>
           )}
-          {nr_tel_1 && (
+          {(nr_tel_1 || nr_tel_2) && (
             <div className="flex gap-2">
-              <p className="text-black dark:text-white font-bold">Тел:</p>
-              <p className="text-black dark:text-white">{nr_tel_1}</p>
+              <p className="text-black dark:text-white font-bold w-3/12">{t('pages_category.developers.page_item.tel')}:</p>
+              <div className="flex flex-col">
+                {nr_tel_1 && <p className="text-black dark:text-white">{nr_tel_1}</p>}
+                {nr_tel_2 && <p className="text-black dark:text-white">{nr_tel_2}</p>}
+              </div>
             </div>
           )}
-          {nr_tel_2 && (
+
+          {(email_1 || email_2) && (
             <div className="flex gap-2">
-              <p className="text-black dark:text-white font-bold">Тел:</p>
-              <p className="text-black dark:text-white">{nr_tel_2}</p>
-            </div>
-          )}
-          {email_1 && (
-            <div className="flex gap-2">
-              <p className="text-black dark:text-white font-bold">Email:</p>
-              <p className="text-black dark:text-white">{email_1}</p>
-            </div>
-          )}
-          {email_2 && (
-            <div className="flex gap-2">
-              <p className="text-black dark:text-white font-bold">Email:</p>
-              <p className="text-black dark:text-white">{email_2}</p>
+              <p className="text-black dark:text-white font-bold w-3/12">{t('pages_category.developers.page_item.email')}:</p>
+              <div className="flex flex-col">
+                {email_1 && <p className="text-black dark:text-white">{email_1}</p>}
+                {email_2 && <p className="text-black dark:text-white">{email_2}</p>}
+              </div>
             </div>
           )}
         </div>
