5fbafa0cf0853f13ff3ab20fdfa14fd63006b077
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-10-04 19:36:29 +0200

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -38,5 +38,25 @@ router.post('/register', async (req, res) => {
     res.status(500).json({ message: 'Something went wrong. Please try again' });
   }
 });
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'User not found' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid password' });
+    }
+
+    const token = jwt.sign({ userId: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.json({ token, userId: user.id, message: 'Login successful' });
+  } catch (err) {
+    res.status(500).json({ message: 'Something went wrong. Please try again' });
+  }
+});
 module.exports = router;
--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+axios.defaults.baseURL = 'http://localhost:3000/api'; // Измените на ваш адрес сервера
 
 const token = {
   set(token) {
@@ -12,59 +12,47 @@ const token = {
   },
 };
 
-export const register = createAsyncThunk(
-  'auth/register',
-  async (userData, { rejectWithValue }) => {
-    try {
-      const { data } = await axios.post('/users/signup', userData);
-      token.set(data.token);
-      return data;
-    } catch (error) {
-      return rejectWithValue(error);
-    }
+export const register = createAsyncThunk('auth/register', async (userData, { rejectWithValue }) => {
+  try {
+    const { data } = await axios.post('/auth/register', userData);
+    token.set(data.token);
+    return data;
+  } catch (error) {
+    return rejectWithValue(error);
   }
-);
+});
 
-export const login = createAsyncThunk(
-  'auth/login',
-  async (userData, { rejectWithValue }) => {
-    try {
-      const { data } = await axios.post('/users/login', userData);
-      token.set(data.token);
-      return data;
-    } catch (error) {
-      return rejectWithValue(error);
-    }
+export const login = createAsyncThunk('auth/login', async (userData, { rejectWithValue }) => {
+  try {
+    const { data } = await axios.post('/auth/login', userData);
+    token.set(data.token);
+    return data;
+  } catch (error) {
+    return rejectWithValue(error);
   }
-);
+});
 
-export const logout = createAsyncThunk(
-  'auth/logout',
-  async (_, { rejectWithValue }) => {
-    try {
-      await axios.post('/users/logout');
-      token.unset();
-    } catch (error) {
-      return rejectWithValue(error);
-    }
+export const logout = createAsyncThunk('auth/logout', async (_, { rejectWithValue }) => {
+  try {
+    await axios.post('/users/logout');
+    token.unset();
+  } catch (error) {
+    return rejectWithValue(error);
   }
-);
+});
 
-export const fetchCurrentUser = createAsyncThunk(
-  'auth/refresh',
-  async (_, { rejectWithValue, getState }) => {
-    const tokenLS = getState().auth.token;
-    if (!tokenLS) {
-      return rejectWithValue('No token');
-    }
-    token.set(tokenLS);
-    try {
-      const { data } = await axios('/users/current');
-      return data;
-    } catch (error) {
-      return rejectWithValue(error);
-    }
+export const fetchCurrentUser = createAsyncThunk('auth/refresh', async (_, { rejectWithValue, getState }) => {
+  const tokenLS = getState().auth.token;
+  if (!tokenLS) {
+    return rejectWithValue('No token');
   }
-);
+  token.set(tokenLS);
+  try {
+    const { data } = await axios('/users/current');
+    return data;
+  } catch (error) {
+    return rejectWithValue(error);
+  }
+});
 
 // getState возвращает весь глобальный стейт.
