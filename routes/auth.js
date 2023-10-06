5e01e15da34d7edf970a362f7d592a88136e5ecf
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-10-06 21:15:19 +0200

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -9,17 +9,22 @@ const router = express.Router();
 
 router.post('/register', async (req, res) => {
   try {
+    console.log('Attempting to register a user...');
+
     const { email, password, name, phone } = req.body;
 
     // Проверка существующего пользователя
     const existingUser = await User.findOne({ email });
     if (existingUser) {
+      console.log('User already exists.');
       return res.status(400).json({ message: 'User with this email already exists' });
     }
 
+    console.log('Hashing password...');
     // Хеширование пароля
     const hashedPassword = await bcrypt.hash(password, 12);
 
+    console.log('Creating new user...');
     // Создание нового пользователя
     const user = new User({
       email,
@@ -29,34 +34,16 @@ router.post('/register', async (req, res) => {
     });
 
     await user.save();
+    console.log('User saved successfully.');
 
     // Генерация токена
+    console.log('Generating JWT token...');
     const token = jwt.sign({ userId: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
+    console.log('Registration completed successfully.');
     res.status(201).json({ token, userId: user.id, message: 'Registration successful' });
   } catch (err) {
+    console.error('Error during registration:', err);
     res.status(500).json({ message: 'Something went wrong. Please try again' });
   }
 });
-router.post('/login', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ message: 'User not found' });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ message: 'Invalid password' });
-    }
-
-    const token = jwt.sign({ userId: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-    res.json({ token, userId: user.id, message: 'Login successful' });
-  } catch (err) {
-    res.status(500).json({ message: 'Something went wrong. Please try again' });
-  }
-});
-module.exports = router;
