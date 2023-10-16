1d7088f3c266549d6241c9ca8682d90832f389e3
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-10-16 16:48:25 +0200

    commit

--- src/pages/Auth/User/Profile/PayPage.jsx
+++ src/pages/Auth/User/Profile/PayPage.jsx
@@ -7,8 +7,10 @@ const PayPage = () => {
       </div>
 
       <ul className="grid grid-cols-3 gap-4">
-        <li className="p-5 bg-white dark:bg-neutral-800 shadow-md">
-          <h3 className="text-center text-[1.25rem] mb-5">Пакет "Base"</h3>
+        <li className="p-5 bg-white dark:bg-neutral-900 shadow-md dark:shadow-white duration-200 hover:scale-110">
+          <h3 className="text-center text-[1.25rem] mb-5 text-sky-500 dark:text-yellow-500">
+            Пакет "Base"
+          </h3>
           <ul className="list-none ">
             <li className="flex justify-start items-center gap-4 border-b border-neutral-200 dark:border-neutral-500 py-2">
               <LuCheck className="text-green-600" />
@@ -31,7 +33,69 @@ const PayPage = () => {
               <p className="text-[1rem] text-black dark:text-white">Опция 1</p>
             </li>
           </ul>
-          <button className="w-full justify-center items-center border border-green-600 bg-white duration-200 hover:bg-green-600 text-green-600 hover:text-white py-2 px-4 mt-5">
+          <button className="w-full justify-center items-center border border-green-600 dark:border-yellow-500 bg-white dark:bg-transparent duration-200 hover:bg-green-600 dark:hover:bg-yellow-500 text-green-600 dark:text-white hover:text-white py-2 px-4 mt-5">
+            Оформить
+          </button>
+        </li>
+
+        <li className="p-5 bg-white dark:bg-neutral-900 shadow-md dark:shadow-white duration-200 hover:scale-110">
+          <h3 className="text-center text-[1.25rem] mb-5 text-sky-500 dark:text-yellow-500">
+            Пакет "Advanced"
+          </h3>
+          <ul className="list-none ">
+            <li className="flex justify-start items-center gap-4 border-b border-neutral-200 dark:border-neutral-500 py-2">
+              <LuCheck className="text-green-600" />
+              <p className="text-[1rem] text-black dark:text-white">Опция 1</p>
+            </li>
+            <li className="flex justify-start items-center gap-4 border-b border-neutral-200 dark:border-neutral-500 py-2">
+              <LuCheck className="text-green-600" />
+              <p className="text-[1rem] text-black dark:text-white">Опция 1</p>
+            </li>
+            <li className="flex justify-start items-center gap-4 border-b border-neutral-200 dark:border-neutral-500 py-2">
+              <LuCheck className="text-green-600" />
+              <p className="text-[1rem] text-black dark:text-white">Опция 1</p>
+            </li>
+            <li className="flex justify-start items-center gap-4 border-b border-neutral-200 dark:border-neutral-500 py-2">
+              <LuX className="text-red-500" />
+              <p className="text-[1rem] text-black dark:text-white">Опция 1</p>
+            </li>
+            <li className="flex justify-start items-center gap-4 py-2">
+              <LuX className="text-red-500" />
+              <p className="text-[1rem] text-black dark:text-white">Опция 1</p>
+            </li>
+          </ul>
+          <button className="w-full justify-center items-center border border-green-600 dark:border-yellow-500 bg-white dark:bg-transparent duration-200 hover:bg-green-600 dark:hover:bg-yellow-500 text-green-600 dark:text-white hover:text-white py-2 px-4 mt-5">
+            Оформить
+          </button>
+        </li>
+
+        <li className="p-5 bg-white dark:bg-neutral-900 shadow-md dark:shadow-white duration-200 hover:scale-110">
+          <h3 className="text-center text-[1.25rem] mb-5 text-sky-500 dark:text-yellow-500">
+            Пакет "Pro"
+          </h3>
+          <ul className="list-none ">
+            <li className="flex justify-start items-center gap-4 border-b border-neutral-200 dark:border-neutral-500 py-2">
+              <LuCheck className="text-green-600" />
+              <p className="text-[1rem] text-black dark:text-white">Опция 1</p>
+            </li>
+            <li className="flex justify-start items-center gap-4 border-b border-neutral-200 dark:border-neutral-500 py-2">
+              <LuCheck className="text-green-600" />
+              <p className="text-[1rem] text-black dark:text-white">Опция 1</p>
+            </li>
+            <li className="flex justify-start items-center gap-4 border-b border-neutral-200 dark:border-neutral-500 py-2">
+              <LuCheck className="text-green-600" />
+              <p className="text-[1rem] text-black dark:text-white">Опция 1</p>
+            </li>
+            <li className="flex justify-start items-center gap-4 border-b border-neutral-200 dark:border-neutral-500 py-2">
+              <LuX className="text-red-500" />
+              <p className="text-[1rem] text-black dark:text-white">Опция 1</p>
+            </li>
+            <li className="flex justify-start items-center gap-4 py-2">
+              <LuX className="text-red-500" />
+              <p className="text-[1rem] text-black dark:text-white">Опция 1</p>
+            </li>
+          </ul>
+          <button className="w-full justify-center items-center border border-green-600 dark:border-yellow-500 bg-white dark:bg-transparent duration-200 hover:bg-green-600 dark:hover:bg-yellow-500 text-green-600 dark:text-white hover:text-white py-2 px-4 mt-5">
             Оформить
           </button>
         </li>
